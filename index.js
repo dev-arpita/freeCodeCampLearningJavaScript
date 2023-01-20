252f2a174307921aabd60b3cb11fe1211bd0e664
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-20 11:18:42 +0530

    Iterate with JavaScript Do...While Loops

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function multiplyAll(arr) {
-  let product = 1;
-  // Only change code below this line
-  for( let i = 0; i < arr.length; i++) {
-    for( let a = 0; a < arr[i].length; a++) {
-      console.log(product *= arr[i][a])
+// Setup
+const myArray = [];
+let i = 10;
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 10);
 
-    }
-  }
-  // Only change code above this line
-  return product;
-}
-
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+console.log(myArray);
