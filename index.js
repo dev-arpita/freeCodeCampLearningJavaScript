84a3c87226058a4946b11ef6f331e8e54a726f0a
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-25 19:04:07 +0530

    Use Recursion to Create a Range of Numbers

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-function countdown(n){
-  if (n < 1) {
-    return [];
-  } else {
-    const countArray = countdown(n - 1);
-    countArray.unshift(n)
-  return countArray;
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum)
+  return [];
+    else {
+    const numArr = rangeOfNumbers(startNum, endNum - 1)
+      numArr.push(endNum)
+    return numArr
   }
-}
-console.log(countdown(5));
+};
+console.log(rangeOfNumbers(1, 10))
 
 
