a3c9a1f384670624c3c8947d4d2b14596ad29249
Arpita Roy <arpita.roy.workar@gmail.com>
2023-02-16 18:36:49 +0530

    Remove Whitespace from Start and End

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-let str = "one two three";
-let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
-let replaceText = "$3 $2 $1"; // Change this line
-let result = str.replace(fixRegex, replaceText);
-console.log(result)
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    let row = [];
+
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
