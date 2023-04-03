6607cb9011fe0f20c17ee94b052d7d32d59121e2
Arpita Roy <arpita.roy.workar@gmail.com>
2023-04-03 19:17:11 +0530

    Understand Functional Programming Terminology

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const funModule = (()=> {
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
 
- return {
-   isCuteMixin: function(obj) {
-    obj.isCute = function() {
-      console.log(true);
-    };
-  },
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
 
-  singMixin: function(obj) {
-    obj.sing = function() {
-      console.log("Singing to an awesome tune");
-    };
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
   }
+  return teaCups;
+};
 
- };
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
 
-})();
-console.log(funModule)
-
-console.log(funModule.isCuteMixin)
-console.log(funModule.singMixin)
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
