70f4ba8bec5a1bfcf2d8c297075b4bd3e449c25d
Arpita Roy <arpita.roy.workar@gmail.com>
2023-06-15 19:38:14 +0530

    missing-letters

--- 09-10-Missing-letters/index.js
+++ 09-10-Missing-letters/index.js
@@ -1,5 +1,13 @@
 function fearNotLetter(str) {
-  return str;
+  for(let i =0; i < str.length; i++){
+    const charCode = str.charCodeAt(i)
+    // console.log(str.charCodeAt(0) + i)
+    if(charCode !== str.charCodeAt(0) + i) {
+      return String.fromCharCode(charCode - 1)
+    }
+  }
+  return undefined;
 }
 
-fearNotLetter("abce");
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
