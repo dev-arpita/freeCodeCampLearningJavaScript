a389bc45079be4037ceb1b8d17c262d710d2e55e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-08-23 13:52:58 +0530

    update changes

--- all-odd-fibonacci/index.js
+++ all-odd-fibonacci/index.js
@@ -4,14 +4,20 @@ function sumFibs(num) {
   let result = 0;
   while (currNumber <= num) {
     if (currNumber % 2 !== 0) {
+      console.log("C",currNumber)
+      console.log("RB",result)
       result += currNumber;
+      console.log("R",result)
     }
+    // Update next number
     currNumber += prevNumber;
+     console.log("C",currNumber)
+    // Update previous number
     prevNumber = currNumber - prevNumber;
+     console.log("P",prevNumber)
   }
-
   return result;
 }
 
 // test here
-sumFibs(4);
+console.log(sumFibs(4));
